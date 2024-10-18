fce33f22c97cdc73973d0e07e04c2d11078437b5
Diki Saputra <diki@CNCRD.LAN>
2024-10-18 14:09:34 +0700

    update id lightening

--- app/Http/Controllers/SubincidenttypeController.php
+++ app/Http/Controllers/SubincidenttypeController.php
@@ -110,7 +110,8 @@ class SubincidenttypeController extends Controller
                         ->orWhere('wp_terms.term_id', 16203)
                         ->orWhere('wp_terms.term_id', 16204)
                         ->orWhere('wp_terms.term_id', 16205)
-                        ->orWhere('wp_terms.term_id', 16206);
+                        ->orWhere('wp_terms.term_id', 16206)
+                        ->orWhere('wp_terms.term_id', 18627);
             })
             ->get();
 
